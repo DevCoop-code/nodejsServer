8ab5777aa1d58c837cf80431a8ea3adbdd6b2199
HanGyo <tkdydwkdlfma90@gmail.com>
2020-06-04 10:07:07 +0000

    feat: Add Function about Sign up

--- app.js
+++ app.js
@@ -86,7 +86,36 @@ var authUser = function(database, id, password, callback)
     });
 }
 
-app.post('/process/login', function(req, res){
+// Add Users
+var addUser = function(database, id, password, name, callback)
+{
+    console.log('Called addUser : ' + id + ', ' + password + ', ' + name);
+
+    // Reference 'users' collection
+    var users = database.collection('users');
+
+    users.insertMany([{"id":id, "password":password, "name":name}], function(err, result) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+
+        if (result.insertedCount > 0) {
+            console.log("Add User Record: " + result.insertedCount);
+        } else {
+            console.log("There is Record");
+        }
+
+        callback(null, result);
+    });
+}
+
+// Router
+var router = express.Router();
+
+router.route('/process/login').post(function(req, res){
+    console.log('Called /process/login');
+
     console.log('Called /process/login');
 
     var paramId = req.body.id;
@@ -116,12 +145,38 @@ app.post('/process/login', function(req, res){
     }
 });
 
-// Router
-var router = express.Router();
+router.route('/process/addUser').post(function(req, res) {
+    console.log('Called /process/addUser');
 
-router.route('/process/login').post(function(req, res){
-    console.log('/process/login 호출됨');
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    var paramName = req.body.name || req.query.name;
+
+    console.log('Requested Parameter : ' + paramId + ', ' + paramPassword + ', ' + paramName);
+
+    if (database) {
+        addUser(database, paramId, paramPassword, paramName, function(err, result){
+            if (err) {throw err;}
+
+            if (result && result.insertedCount > 0) {            // Success to add user
+                console.dir(result);
+
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>Success to User Add</h2>');
+                res.end();
+            } else {        // Fail to add user
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>Fail to User Add</h2>');
+                res.end();
+            }
+        });
+    } else {        // Database is not initialized
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h2>Fail to Connect DB</h2>');
+        res.end();
+    }
 });
+
 
 // Router 객체 등록
 app.use('/', router)
